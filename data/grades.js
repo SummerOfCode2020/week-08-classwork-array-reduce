647a170e51544dcadedd7d69bd177a7022c124a5
Jessica <reynolds.jessann@gmail.com>
2020-07-23 19:11:11 -0400

    Change scores to numbers not strings

--- data/grades.js
+++ data/grades.js
@@ -2,27 +2,27 @@ let grades = [
     {
         assignmentName: 'Assignment 01',
         studentName: 'Tito',
-        score: '85',
+        score: 85,
     },
     {
         assignmentName: 'Assignment 02',
         studentName: 'Ralph',
-        score: '74',
+        score: 74,
     },
     {
         assignmentName: 'Assignment 03',
         studentName: 'Ralph',
-        score: '90',
+        score: 90,
     },
     {
         assignmentName: 'Assignment 03',
         studentName: 'Tito',
-        score: '95',
+        score: 95,
     },
     {
         assignmentName: 'Assignment 04',
         studentName: 'Ralph',
-        score: '85',
+        score: 85,
     },
 ]
 module.exports = {
