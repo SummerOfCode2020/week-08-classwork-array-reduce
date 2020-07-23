467592162107b399c2aa3ca171287b198a470474
Yor Name <hollisjamison@gmail.com>
2020-07-23 18:54:38 -0400

    Creates grade array in grades.js

---
+++ data/grades.js
@@ -0,0 +1,32 @@
+modules.exports = [
+    {
+        assignmentName: 'Assignment 1',
+        studentName: 'Tito',
+        score: 50
+    },
+    {
+        assignmentName: 'Assignment 1',
+        studentName: 'Hollis',
+        score: 100
+    },
+    {
+        assignmentName: 'Assignment 2',
+        studentName: 'Tito',
+        score: 75
+    },
+    {
+        assignmentName: 'Assignment 2',
+        studentName: 'Hollis',
+        score: 80
+    },
+    {
+        assignmentName: 'Assignment 3',
+        studentName: 'Tito',
+        score: 95
+    },
+    {
+        assignmentName: 'Assignment 3',
+        studentName: 'Hollis',
+        score: 100
+    }
+]
