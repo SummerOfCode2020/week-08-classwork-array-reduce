5d7d20f9655e596bdc6bf5dc28263c7c8265a96c
mvrcusj <mvrcusj@gmail.com>
2020-07-23 19:02:11 -0400

    Initialize an array with 5 objects and 3 properties within each

---
+++ data.js/grades.js
@@ -0,0 +1,25 @@
+const grades = [{
+    assignmentName: 'calculator',
+    studentName: 'kevin',
+    score: '65'
+}, {
+    assignmentName: 'fantasy',
+    studentName: 'lisa',
+    score: '78'
+}, {
+    assignmentName: 'hazy',
+    studentName: 'kevin',
+    score: '90'
+}, {
+    assignmentName: 'challenge',
+    studentName: 'lisa',
+    score: '82'
+}, {
+    assignmentName: 'perfect',
+    studentName: 'lisa',
+    score: '96'
+}]
+
+module.exports = {
+    grades
+}
