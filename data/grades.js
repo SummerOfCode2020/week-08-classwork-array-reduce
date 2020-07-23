5d3718cf58e0ae1d6352aaeffce1dc2ec62ddcf4
Peter Harrington <pharrington375@gmail.com>
2020-07-23 18:52:09 -0400

    Init grades array in grades.js

---
+++ data/grades.js
@@ -0,0 +1,25 @@
+assignmentName
+studentName
+score
+
+let grades = [{
+    assignmentName: 'Array Reverser',
+    studentName: 'Peter',
+    score: 95
+}, {
+    assignmentName: 'Number Summer',
+    studentName: 'Julie',
+    score: 100
+}, {
+    assignmentName: 'Area Finder',
+    studentName: 'Nick',
+    score: 100
+}, {
+    assignmentName: 'Week04',
+    studentName: 'Alvin',
+    score: 99
+}, {
+    assignmentName: 'Week05',
+    studentName: 'Jessica',
+    score: 98
+}]
