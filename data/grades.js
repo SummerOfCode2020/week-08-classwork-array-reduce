f5e50d2828f1310039af95f718c1902cee85dd4f
Sharon Lin <hsiang.lin@gmail.com>
2020-07-23 18:57:19 -0400

    Complete grades file in data folder

---
+++ data/grades.js
@@ -0,0 +1,39 @@
+// Declare and initialize an array named `grades`
+    
+// Include at least 5 object items in the array 
+
+// Each object will have properties of
+//     - `assignmentName`  with string values such as "Assignment 01"
+//     - `studentName` with string values such as "Tito"
+//     - `score` with numeric values such as 50
+
+// Values are important for this lesson.
+
+//     - Use 2 distinct assignment names
+//     - Use 2 distinct student names
+
+let grades = [{
+  assignmentName: 'Assignment 01', 
+  studentName: 'Annabelle',
+  score: 90 
+}, { 
+  assignmentName: 'Assignment 01',
+  studentName: 'Adrian',
+  score: 86
+}, {
+  assignmentName: 'Assignment 02',
+  studentName: 'Annabelle',
+  score: 91
+}, {
+  assignmentName: 'Assignment 02',
+  studentName: 'Adrian',
+  score: 84
+}, {
+  assignmentName: 'Assignment 03',
+  studentName: 'Annabelle',
+  score: 87
+}, {
+  assignmentName: 'Assignment 03',
+  studentName: 'Adrian',
+  score: 89
+}]
