904e035b574fec4e356a82a33b9bc5cdf86aa74f
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-23 18:53:13 -0400

    Create grades array for reduce lesson

---
+++ data/grades.js
@@ -0,0 +1,30 @@
+grades = [{
+    assignmentName: Assignment01,
+    studentName: Timmy,
+    score: 90
+},
+{
+    assignmentName: Assignment01,
+    studentName: Zoey,
+    score: 95
+},
+{
+    assignmentName: Assignment02,
+    studentName: Timmy,
+    score: 85
+},
+{
+    assignmentName: Assignment02,
+    studentName: Zoey,
+    score: 70
+},
+{
+    assignmentName: Assignment03,
+    studentName: Barry,
+    score: 100
+},
+{
+    assignmentName: Assignment03,
+    studentName: Timmy,
+    score: 60
+}]
