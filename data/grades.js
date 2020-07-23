1aa03a229a70137fa3ec0d222fafdc496d4ae6c3
Nick Lavery <tuneboynick@gmail.com>
2020-07-23 18:53:38 -0400

    First commit including data and export

---
+++ data/grades.js
@@ -0,0 +1,40 @@
+let grades = [{
+    assignmentName: 'Assignment01',
+    studentName: 'Seven',
+    score: 86
+}, {
+    assignmentName: 'Assignment02',
+    studentName: 'Spencer',
+    score: 71
+}, {
+    assignmentName: 'Assignment03',
+    studentName: 'John',
+    score: 90
+}, {
+    assignmentName: 'Assignment04',
+    studentName: 'Geneva',
+    score: 99
+}, {
+    assignmentName: 'Assignment05',
+    studentName: 'Derrik',
+    score: 62
+}, {
+    assignmentName: 'Assignment06',
+    studentName: 'Paul',
+    score: 87
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    grades
+}
